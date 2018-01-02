b319025f8df4f131126f5cb6bed43671777e3287
qiuzhujun <jochenshi@gmail.com>
2018-01-02 18:25:44 +0800

    ascription info

--- src/control/ascription.js
+++ src/control/ascription.js
@@ -5,6 +5,14 @@ const model = require('../models');
 const methods = require('../common/methods');
 const errorText = require('../common/error');
 
+
+
+
+/**
+ * 添加出入记录信息
+ * @param param
+ * @returns {Promise.<boolean>}
+ */
 const addAscription = async (param) => {
     try{
         await model.ascription.create({
@@ -45,5 +53,16 @@ const modifyAscription = async(param) => {
         return false;
     }
 }
+
+/**
+ * 得到所有还在库存中的设备归属信息
+ * @param res
+ * @returns {Promise.<void>}
+ */
+const getInAscription = async(res) => {
+
+}
+
+
 
 module.exports = {addAscription,modifyAscription}
